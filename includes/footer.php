2f09a7d917432f75e863ed22cf4d1b83f81d8858
Carduelis <pavepy@yandex.ru>
2015-09-03 21:02:53 +0300

    правильные пути до jq

--- includes/footer.php
+++ includes/footer.php
@@ -6,9 +6,6 @@
   </div>
 </footer>
 
-<!--css only modal-->
-<!--http://drublic.github.io/css-modal/#!-->
-
 
 <script src="js/jquery-2.1.4.min.js"></script>
 <script src="js/functions.js" type="text/javascript"></script>

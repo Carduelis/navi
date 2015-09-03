f4d1bf884eaa6aad8e04714ea319baa707f08f94
Carduelis <pavepy@yandex.ru>
2015-09-03 20:59:04 +0300

    Лишние иконки и последний jQuery

--- includes/footer.php
+++ includes/footer.php
@@ -10,15 +10,8 @@
 <!--http://drublic.github.io/css-modal/#!-->
 
 
-<script src="js/jquery.min.js"></script>
+<script src="js/jquery-2.1.4.min.js"></script>
 <script src="js/functions.js" type="text/javascript"></script>
-
-<!--slider-->
-<link rel="stylesheet" href="js/cs3/idangerous.chopslider-3.4.css"/>
-<script src="js/cs3/idangerous.chopslider-3.4.min.js"></script>
-<!--end slider-->
-<script src="js/bxslider/jquery.bxslider.min.js"></script>
-<!--<script src="js/jquery.fittext.js"></script>-->
 
 
 

b382c7b7a150f734a7ae91af119e21d63d5b07ff
Carduelis <pavepy@yandex.ru>
2015-11-14 13:38:28 +0300

    Формы

--- includes/forms/link_roomnames_to_id.php
+++ includes/forms/link_roomnames_to_id.php
@@ -1,27 +1,14 @@
 <form name="link_roomnames_to_id" method="post">
-<table>
-	<tr>
-		<td>
-			<label for="tagId">id данного тега</label>
-		</td>
-		<td>
-			<input type="text" name="tagId" id="tagId" disabled="disabled">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<label for="roomName">Введите название помещения</label>
-		</td>
-		<td>
-			<input type="text" name="roomName" id="roomName">
-		</td>
-	</tr>
-	<tr>
-		<td></td>
-		<td><a class="simple-btn" onclick="sendData(['tagId','roomName'])">Сохранить в БД</a>
-	</tr>
-	<tr>
-		<td></td>
-		<td id="message"></td>
-	</tr>
+	<div class="form-group">
+		<label for="tagId">Внутренний id данного объекта</label>
+		<input type="text" name="tagId" id="tagId" disabled="disabled">
+	</div>
+	<div class="form-group">
+		<label for="roomName">Введите название помещения</label>
+		<input type="text" name="roomName" id="roomName">	
+	</div>
+	<div class="form-group">
+		<a class="simple-btn" onclick="sendData(['tagId','roomName'])">Сохранить в БД</a>
+		<div id="message"></div>
+	</div>
 </form>
---
+++ includes/forms/link_users_to_rooms.php
@@ -0,0 +1,17 @@
+<form name="link_users_to_rooms" method="post">
+	<div class="form-group">
+		<label for="tagId">Внутренний id объекта</label>
+		<input type="text" name="tagId" id="tagId" disabled="disabled">
+	</div>
+	<div class="form-group">
+		<label for="userId">Выберите сотрудника для привязки к помещению</label>
+		<select id="userId">
+			<option value="">Иванов Иван</option>
+			<option value="">Петров Петр</option>
+		</select>
+	</div>
+	<div class="form-group">
+		<a class="simple-btn" onclick="sendData(['tagId','userId'])">Сохранить в БД</a>
+		<div id="message"></div>
+	</div>
+</form>
